803cd13809bf0ba6d44207be37cd7bbacf407ee1
Irskaraieva <mishytkina14@gmail.com>
2024-04-22 21:13:36 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+// import hello from "./modules/hello";
+// import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
+
+// new AirDatepicker('#date');
+
+// console.log(hello);
+import mobileNav from './modules/mobile-nav.js';
+import footerLists from './modules/footer.js';
+const nav = document.getElementById('nav');
+const navMobBtn = document.getElementById('nav-mob-btn');
+const navLinks = document.querySelectorAll('.nav-list li');
+
+mobileNav();
+footerLists();
+
+navLinks.forEach(li => {
+    li.addEventListener('click', () => {
+        if (window.innerWidth < 1000) {
+            navMobBtn.classList.toggle('active');
+            document.body.classList.remove('no-scroll');
+        }
+    });
+});
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 1000) {
+        document.body.classList.remove('no-scroll');
+        navMobBtn.classList.remove('active');
+        nav.style.display = 'flex'
+    } else if ((!navMobBtn.classList.contains('active')) && (window.innerWidth < 1000)) {
+        nav.style.display = 'none';
+    }
+});
+
+
+
+
+
+
